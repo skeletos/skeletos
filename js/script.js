a4f2f8227ef4187476c3e1092a647833b0251c75
Tony Camaiani <tony@peertransfer.com>
2012-11-15 16:12:48 +0100

    coderetreat version

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 /* Author:
 
 */
-
+jQuery(function() {
+  return $("a.scroll").click(function() {
+    $("html,body").animate({
+      scrollTop: $(this.hash).offset().top
+    }, 750);
+    return false;
+  });
+});
 
 
 
